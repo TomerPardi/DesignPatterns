9429bed1b6d8c8865d0089681207c3575766aa3d
Tomer Pardilov <tomer.pardilov@live.biu.ac.il>
2022-03-16 21:56:24 +0200

    Add project files.

---
+++ DesignPatterns/Singleton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    internal class Singleton
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
